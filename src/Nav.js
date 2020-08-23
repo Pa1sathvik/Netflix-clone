5a8f262ad320649cdb1fbc997b02a2458fbc0812
Vutukuri <svutukuri@informatica.com>
2020-08-23 17:43:10 +0530

    Added header

---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import avatar from "./images/avatar.png";
+import logo from "./images/Netflixlogo.png";
+
+function Nav() {
+
+    const[show,handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img className="nav__logo" src={logo} alt="Netflix logo" />
+
+      <img className="nav__avatar" src={avatar} alt="Netflix logo" />
+    </div>
+  );
+}
+
+export default Nav;
