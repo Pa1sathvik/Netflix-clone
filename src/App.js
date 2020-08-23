ed385f0308d715111d74972c3fe7d70b6961ee6d
Vutukuri <svutukuri@informatica.com>
2020-08-23 14:22:51 +0530

    Mid way

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Row from './Row'
+import requests from './requests'
 
 function App() {
   return (
     <div className="App">
      <h1>IMDBdir
      </h1>
+     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchRomanceMovies}/>
+     <Row title="Trending Now" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   );
 }
---
+++ src/requests.js
@@ -0,0 +1,17 @@
+const API_KEY = "ffe5f7b2f0ea5f8b54612eb65cb9d537";
+
+const requests = {
+
+    fetchTrending:`/trending/alt/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/movie?api_key=${API_KEY}with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en—US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+
+}
+
+export default requests;
